6f13e6807e8c6223487eef87b781a9b911b22d46
Isaac Carrera <rolodef1@gmail.com>
2017-12-13 18:15:40 -0500

    Add link video

--- resources/views/emails/notificarAmigoSecreto.blade.php
+++ resources/views/emails/notificarAmigoSecreto.blade.php
@@ -19,13 +19,13 @@
 			<h3>El sorteo ha sido realizado y tu amigo secreto a quien debes entregar el regalo es "{{$amigo_secreto->nombre}}"</h3>
 		</p>
 		<p>
-			<h4>Asi como a tu tienes un amigo secreto a quien entregar el regalo, tambien hay alguien del grupo que tiene que darte un regalo a ti</h4>
+			<h4>Asi como tu tienes un amigo secreto a quien entregar el regalo, tambien hay alguien del grupo que tiene que darte un regalo a ti</h4>
 		</p>
 		<p>
 			<h4>Para hacer que este juego sea mucho más facil y tanto tu como tus amigos reciban los regalos que les gustan y necesitan, puedes seleccionar varias opciones de regalo en ETAFASHION.COM y agregarlas a tu lista de deseos</h4>
 		</p>
 		<p>
-			<h4>Una vez que hayas llenado tu lista de deseos tu amigo sera notificado de los productos que podria regalarte.</h4>
+			<h4>Una vez que hayas llenado tu lista de deseos tu amigo sera notificado de los productos que podria regalarte, <a href="https://www.etafashion.com/amigo-secreto">ingresa aqui para aprender como llenar tu lista de deseos</a>.</h4>
 		</p>
 	</div>
 </body>
