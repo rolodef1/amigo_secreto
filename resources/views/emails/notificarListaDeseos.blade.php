d2cbcc97ca8c24706def48ec39e7d5af71ee68fb
Isaac Carrera <rolodef1@gmail.com>
2017-12-13 14:52:40 -0500

    Update emails

--- resources/views/emails/notificarListaDeseos.blade.php
+++ resources/views/emails/notificarListaDeseos.blade.php
@@ -3,18 +3,78 @@
 <head>
 	<meta charset="utf-8">
 </head>
-<body>	
-	<div style="text-align: center;">
-		<img src="{{ asset('images/banner-mail.jpg') }}">
-		<p>
-			<h1>Hola {{$integrante->nombre}}</h1>
-		</p>
-		<p>
-			<h2>Tu amigo secreto a creado su lista de deseos</h2>
-		</p>
-		<p>
-			<h2>Ingresa a <a href="https://www.etafashion.com/wishlist/shared/index/code/{{$shared_code}}/">este link</a> para ver los productos que le gustaria que le regalen</h2>
-		</p>		
-	</div>
+<body style="width: 600px;">	
+	<table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px; background:#FFFFFF;">
+		<tr>
+			<td style="padding: 10px 0; text-align: center"><a href="https://www.etafashion.com/?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays"><img src="images/1camisas_09.jpg" aria-hidden="true" width="400" height="123" alt="ETAFASHION.COM" border="0" style="height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;"></a>
+			</td>
+		</tr>
+	</table>
+
+	<table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 600px; background:#FFFFFF; border-bottom:1px solid #e6e7e8; border-top:1px solid #e6e7e8;">
+		<tr height="38">
+			<td width="10%" align="center"><a href="https://etafashion.com/ropa-mujer.html?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays" target="_blank"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/marzo/lunes_rosa/images/menuEmail_01.jpg" width="56" alt="Mujer" border="0" class="g-img" style="width: 100%; max-width: 56px; height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true"/></a></td>
+			<td width="12%" align="center"><a href="https://www.etafashion.com/hombre.html?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays" target="_blank"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/marzo/lunes_rosa/images/menuEmail_02.jpg" width="71" alt="Hombre" border="0"class="g-img" style="width: 100%; max-width: 71px; height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true"/></a></td>
+			<td width="10%" align="center"><a href="https://www.etafashion.com/infantil.html?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays" target="_blank"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/marzo/lunes_rosa/images/menuEmail_03.jpg" width="61" alt="Niños" border="0" class="g-img" style="width: 100%; max-width: 61px; height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true"/></a></td>
+			<td width="12%" align="center"><a href="https://www.etafashion.com/zapatos.html?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays" target="_blank"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/marzo/lunes_rosa/images/menuEmail_04.jpg" width="71" alt="Zapatos" border="0" class="g-img" style="width: 100%; max-width: 71px; height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true"/></a></td>
+			<td width="15%" align="center"><a href="https://www.etafashion.com/accesorios.html?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays" target="_blank"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/marzo/lunes_rosa/images/menuEmail_05.jpg" width="88" alt="Accesorios" border="0" class="g-img" style="width: 100%; max-width: 88px; height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true"/></a></td>
+			<td width="15%" align="center"><a href="https://www.etafashion.com/etabeauty.html?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays" target="_blank"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/marzo/lunes_rosa/images/menuEmail_06.jpg" width="91" alt="ETABauty" border="0" class="g-img" style="width: 100%; max-width: 91px; height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true"/></a></td>
+			<td width="16%" align="center"><a href="https://www.etafashion.com/etahome.html?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays" target="_blank"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/marzo/lunes_rosa/images/menuEmail_07.jpg" width="93" alt="ETAHome" border="0" class="g-img" style="width: 100%; max-width: 93px; height: auto; background: #ffffff; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true"/></a></td>
+		</tr>
+		<tr>
+			<a href="https://www.etafashion.com/wishlist/shared/index/code/{{$shared_code}}/">
+				<img src="{{ asset('images/banner-mail2.jpg') }}">
+			</a>
+		</tr>
+		<tr>
+			<td bgcolor="#ffffff">
+				<img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/noviembre/CyberMonday6/images/email-diferido1.jpg" alt="Difiere tus compras hasta 12 Meses con Crédito Directo ETAFASION" aria-hidden="true" width="600" height="" border="0" align="center" style="width: 100%; max-width: 600px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" class="g-img">
+			</td>
+		</tr>
+
+		<tr>
+			<td bgcolor="#ffffff">
+				<img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/octubre/3X2/images/Beneficios-Compra-ETAFASHIONCOM1.jpg" alt="Consulta el estado de tus pedidos por Whatsapp al 0994531112 (Lun-Vie de 9 a 18 horas) Primera compra 20% de DSCTO. Pago Seguro: Crédito Directo, Efectivo y Tarjeta de Crédito." aria-hidden="true" width="600" height="100" border="0" align="center" style="width: 100%; max-width: 600px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" class="g-img">
+			</td>
+		</tr>
+		<tr>
+			<td bgcolor="#ffffff">
+				<img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/octubre/3X2/images/Beneficios-Compra-ETAFASHIONCOM2.jpg" alt="Entrega a domicilio de 3 a 5 días laborables. Retiro: en tiendas físicas sin costo. Cambio o devolución en tiendas físicas y sin recargo." aria-hidden="true" width="600" height="100" border="0" align="center" style="width: 100%; max-width: 600px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" class="g-img">
+			</td>
+		</tr>		
+	</table>
+	<table role="presentation" aria-hidden="true" cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="max-width: 680px; background: #000000">
+		<tr>
+			<td style="padding:20px 0 10px 0" align="center">
+				<table border="0" cellpadding="0" cellspacing="0">
+					<tr>
+						<td><a href="https://www.facebook.com/ETAFASHIONECUADOR"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/redes-sociales/redessociales_01.jpg" alt="Facebook" width="60" height="" class="g-img" style="width: 100%; max-width: 60px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true" border="0"></a></td>
+						<td><a href="https://twitter.com/etafashion"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/redes-sociales/redessociales_02.jpg" width="50" height="" class="g-img" style="width: 100%; max-width: 50px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true" alt="Twitter"></a></td>
+						<td><a href="https://www.instagram.com/etafashion_ec/"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/redes-sociales/redessociales_03.jpg" width="50" class="g-img" style="width: 100%; max-width: 50px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true" height="" alt="Instagram"></a></td>
+						<td><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/redes-sociales/redessociales_04.jpg" width="50" class="g-img" style="width: 100%; max-width: 50px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true" height="" alt="Snapchat"></td>
+						<td><a href="https://www.etafashion.com/blog?utm_source=EmailEcommerce&utm_medium=Footer&utm_term=10Prendas10LooksTaxiCanguil&utm_content=Blog&utm_campaign=eCommerce-email"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/redes-sociales/redessociales_05.jpg" width="50" height="" class="g-img" style="width: 100%; max-width: 50px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true" alt="Blog"></a></td>
+						<td><a href="https://www.pinterest.com/etafashion_eta/"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/redes-sociales/redessociales_06.jpg" width="50" class="g-img" style="width: 100%; max-width: 50px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true" height="" alt="Pinterest"></a></td>
+						<td><a href="https://www.etafashion.com/tiendas"><img src="https://www.etafashion.com/ecommerce/emailing/ecuador/2017/redes-sociales/redessociales_07.jpg" width="52" class="g-img" style="width: 100%; max-width: 50px; height: auto; background: #dddddd; font-family: sans-serif; font-size: 15px; line-height: 20px; color: #555555;" aria-hidden="true" height="" alt="Tiendas"></a></td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td style="padding: 0px 10px 30px 10px;width: 100%;font-size: 10px;font-family: sans-serif;line-height: 12px;text-align: center;color: #ffffff;" class="x-gmail-data-detectors">
+				Entérate primero de nuestras promociones<br>
+				<webversion style="color:#55aade !important; text-decoration:underline; font-weight: bold;"><a href="https://www.etafashion.com/customer/account/create/?utm_source=Icommkt&utm_medium=Email&utm_campaign=CyberDays" style="color:#55aade !important;">REGÍSTRATE AQUÍ</a></webversion>
+				<br><br>
+				ETAFASHION - Comercial ETATEX C.A. <br>
+				Av Galo Plaza Lasso N60-122 y Santa Lucía<br>
+				Quito - Ecuador<br><br>
+				ETAFASHION © 2017 – Todos los derechos reservados<br>
+				<br>
+				Servicio al cliente: (02) 600 22 00<br />
+				Lunes a Viernes de 09h00 a 20h00<br />
+				Si deseas enviarnos un correo electrónico <a href="mailto:e-shop@etafashion.com" style="color: #55aade"> haz clic aquí</a><br><br>
+				<a href="https://www.etafashion.com" style="color: #ffffff;">Tienda Online </a>· <a href="https://www.etafashion.com/tiendas" style="color: #ffffff;">Tiendas a nivel nacional</a> · <a href="https://www.etafashion.com/politicas" style="color: #ffffff;">Políticas</a> · <a href="https://www.etafashion.com/contacts" style="color: #ffffff;">Contacto</a>
+			</td>
+		</tr>
+	</table>
 </body>
 </html>
